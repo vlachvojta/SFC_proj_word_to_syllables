b728ab4824faa0c29078858b92a57348a1b80d77
vlachvojta <vojtik.vlach@seznam.cz>
2023-11-27 16:57:19 +0100

    Move helper functions to `helpers.py`

--- src/gru_pytorch.py
+++ src/gru_pytorch.py
@@ -93,7 +93,7 @@ def train(train_data: Dataset, val_data: Dataset, learn_rate, hidden_dim=8, epoc
         current_time = time.time()
 
         if epoch % view_step == 0:
-            val_loss_lev, val_out_words, val_labels_words = test_val(model, val_data, device, batch_size)
+            val_loss_lev, val_out_words, val_labels_words = helpers.test_val(model, val_data, device, batch_size)
             val_losses_lev.append(val_loss_lev)
 
             print(f"Epoch {epoch}/{epochs}, trn losses: {trn_losses[-1]:.3f}, {trn_losses_lev[-1]:.2f} %, val losses: {val_losses_lev[-1]:.2f} %")
@@ -105,91 +105,13 @@ def train(train_data: Dataset, val_data: Dataset, learn_rate, hidden_dim=8, epoc
         epoch_times.append(current_time-start_time)
 
         if epoch % save_step == 0:
-            plot_losses(trn_losses, trn_losses_lev, val_losses_lev, hidden_dim, epoch, batch_size, view_step=view_step, path='models')
-            save_model(model, hidden_dim, epoch, batch_size, path='models')
-            save_out_and_labels(val_out_words, val_labels_words, hidden_dim, epoch, batch_size, path='models')
+            helpers.plot_losses(trn_losses, trn_losses_lev, val_losses_lev, hidden_dim, epoch, batch_size, view_step=view_step, path='models')
+            helpers.save_model(model, hidden_dim, epoch, batch_size, path='models')
+            helpers.save_out_and_labels(val_out_words, val_labels_words, hidden_dim, epoch, batch_size, path='models')
 
     print(f"Total Training Time: {sum(epoch_times):.2f} seconds. ({sum(epoch_times)/epochs:.2f} seconds per epoch)")
 
     return model
-
-def save_model(model, hidden_dim, epoch, batch_size, path:str = '.'):
-    model_name = f'torch_gru_{hidden_dim}hid_{batch_size}batch_{epoch}epochs.pt'
-
-    if not os.path.isdir(path):
-        os.makedirs(path)
-
-    torch.save(model.state_dict(), os.path.join(path, model_name))
-    print(f'Model saved to {model_name}')
-
-def test_val(model, val_data, device, batch_size):
-    out_words = []
-    labels_words = []
-
-    for i, (x, labels) in enumerate(val_data.batch_iterator(batch_size, tensor_output=True), start=1):
-        h_start = model.init_hidden(batch_size=batch_size)
-        out, _ = model(x.to(device).float(), h_start.to(device).float())
-        outputs = [charset.tensor_to_word(o) for o in out]
-        labels = [charset.tensor_to_word(l) for l in labels]
-        out_words += outputs
-        labels_words += labels
-
-    out_words = ','.join(out_words)
-    labels_words = ','.join(labels_words)
-
-    return helpers.levenstein_loss(out_words, labels_words), out_words, labels_words
-
-def save_out_and_labels(val_out_words, val_labels_words, hidden_dim, epoch, batch_size, path='models'):
-    if not os.path.isdir(path):
-        os.makedirs(path)
-
-    out_name = f'torch_gru_{hidden_dim}hid_{batch_size}batch_{epoch}epochs_val_out.txt'
-    with open(os.path.join(path, out_name), 'w') as f:
-        f.write(val_out_words)
-
-    labels_name = f'torch_gru_{hidden_dim}hid_{batch_size}batch_{epoch}epochs_val_labels.txt'
-    with open(os.path.join(path, labels_name), 'w') as f:
-        f.write(val_labels_words)
-
-    print(f'Out and labels saved to {out_name} and {labels_name}')
-
-def plot_losses(trn_losses, trn_losses_lev, val_losses_lev, hidden_dim, epoch, batch_size,
-                view_step: int, path:str = '.'):
-    fig, axs = plt.subplots(1, 3, figsize=(15, 5))
-    axs[0].plot(trn_losses)
-    axs[0].set_title('Trn MSE Loss')
-    axs[0].set_xlabel('Epochs')
-    axs[1].plot(trn_losses_lev)
-    axs[1].set_title('Trn Levenshtein Loss')
-    axs[1].set_xlabel('Epochs')
-    axs[2].plot([i * view_step for i in range(len(val_losses_lev))], val_losses_lev)
-    axs[2].set_title('Val Levenshtein Loss')
-    axs[2].set_xlabel('Epochs')
-    plt.tight_layout()
-
-    if not os.path.isdir(path):
-        os.makedirs(path)
-    image_name = f'torch_gru_{hidden_dim}hid_{batch_size}batch_{epoch}epochs_losses.png'
-    fig.savefig(os.path.join(path, image_name))
-
-# def evaluate(model, test_x, test_y, label_scalers):
-#     model.eval()
-#     outputs = []
-#     targets = []
-#     start_time = time.time()
-#     for i in test_x.keys():
-#         inp = torch.from_numpy(np.array(test_x[i]))
-#         labs = torch.from_numpy(np.array(test_y[i]))
-#         h = model.init_hidden(inp.shape[0])
-#         out, h = model(inp.to(device).float(), h)
-#         outputs.append(label_scalers[i].inverse_transform(out.cpu().detach().numpy()).reshape(-1))
-#         targets.append(label_scalers[i].inverse_transform(labs.numpy()).reshape(-1))
-#     print("Evaluation Time: {}".format(str(time.time()-start_time)))
-#     sMAPE = 0
-#     for i in range(len(outputs)):
-#         sMAPE += np.mean(abs(outputs[i]-targets[i])/(targets[i]+outputs[i])/2)/len(outputs)
-#     print("sMAPE: {}%".format(sMAPE*100))
-#     return outputs, targets, sMAPE
 
 
 def main():
@@ -203,9 +125,9 @@ def main():
     print(f'Loaded {len(trn_dataset)} training and {len(val_dataset)} validation samples.')
 
     device = torch.device("cuda") if torch.cuda.is_available() else "cpu"
-    # gru_model = train(trn_dataset, val_dataset, learn_rate=0.001, device=device, batch_size=32, epochs=500, save_step=50, view_step=5, padding=padding)
+    # gru_model = train(trn_dataset, val_dataset, learn_rate=0.001, device=device, batch_size=32, epochs=500, save_step=50, view_step=5)
     gru_model = train(trn_dataset, val_dataset, learn_rate=0.001, device=device, batch_size=32, epochs=2000, save_step=200, view_step=40)
-    # gru_outputs, targets, gru_sMAPE = evaluate(gru_model, test_x, test_y, label_scalers)
+    # gru_outputs, targets, gru_sMAPE = helpers.evaluate(gru_model, test_x, test_y, label_scalers)
 
 
 if __name__ == '__main__':
--- src/helpers.py
+++ src/helpers.py
@@ -1,5 +1,10 @@
-import Levenshtein as lev
+import os
+
+import matplotlib.pyplot as plt
 import torch
+import Levenshtein as lev
+
+import charset
 
 
 def levenstein_loss(a, b):
@@ -11,18 +16,92 @@ def levenstein_loss(a, b):
 
 
 def transpose(data: torch.Tensor):
-    if data.shape[-1] == 1:
-        raise ValueError(f'Tensor already transposed. data.shape: {data.shape}')
-
-    new_shape = [dim for dim in data.shape] + [1]
-    data = torch.reshape(data, new_shape)
-    return data
-
-
-def de_transpose(data: torch.Tensor):
     if not data.shape[-1] == 1:
-        raise ValueError(f'Cannot de_transpose tensor. data.shape: {data.shape}')
+        # Transpose: Add a dimension to the end of the tensor
+        new_shape = [dim for dim in data.shape] + [1]
+        data = torch.reshape(data, new_shape)
+        return data
 
+    # De-transpose: Remove the last dimension of the tensor
     new_shape = [dim for dim in data.shape[:-1]]
     data = torch.reshape(data, new_shape)
     return data
+
+def save_model(model, hidden_dim, epoch, batch_size, path:str = '.'):
+    model_name = f'torch_gru_{hidden_dim}hid_{batch_size}batch_{epoch}epochs.pt'
+
+    if not os.path.isdir(path):
+        os.makedirs(path)
+
+    torch.save(model.state_dict(), os.path.join(path, model_name))
+    print(f'Model saved to {model_name}')
+
+def test_val(model, val_data, device, batch_size):
+    out_words = []
+    labels_words = []
+
+    for i, (x, labels) in enumerate(val_data.batch_iterator(batch_size, tensor_output=True), start=1):
+        h_start = model.init_hidden(batch_size=batch_size)
+        out, _ = model(x.to(device).float(), h_start.to(device).float())
+        outputs = [charset.tensor_to_word(o) for o in out]
+        labels = [charset.tensor_to_word(l) for l in labels]
+        out_words += outputs
+        labels_words += labels
+
+    out_words = ','.join(out_words)
+    labels_words = ','.join(labels_words)
+
+    return levenstein_loss(out_words, labels_words), out_words, labels_words
+
+def save_out_and_labels(val_out_words, val_labels_words, hidden_dim, epoch, batch_size, path='models'):
+    if not os.path.isdir(path):
+        os.makedirs(path)
+
+    out_name = f'torch_gru_{hidden_dim}hid_{batch_size}batch_{epoch}epochs_val_out.txt'
+    with open(os.path.join(path, out_name), 'w') as f:
+        f.write(val_out_words)
+
+    labels_name = f'torch_gru_{hidden_dim}hid_{batch_size}batch_{epoch}epochs_val_labels.txt'
+    with open(os.path.join(path, labels_name), 'w') as f:
+        f.write(val_labels_words)
+
+    print(f'Out and labels saved to {out_name} and {labels_name}')
+
+def plot_losses(trn_losses, trn_losses_lev, val_losses_lev, hidden_dim, epoch, batch_size,
+                view_step: int, path:str = '.'):
+    fig, axs = plt.subplots(1, 3, figsize=(15, 5))
+    axs[0].plot(trn_losses)
+    axs[0].set_title('Trn MSE Loss')
+    axs[0].set_xlabel('Epochs')
+    axs[1].plot(trn_losses_lev)
+    axs[1].set_title('Trn Levenshtein Loss')
+    axs[1].set_xlabel('Epochs')
+    axs[2].plot([i * view_step for i in range(len(val_losses_lev))], val_losses_lev)
+    axs[2].set_title('Val Levenshtein Loss')
+    axs[2].set_xlabel('Epochs')
+    plt.tight_layout()
+
+    if not os.path.isdir(path):
+        os.makedirs(path)
+    image_name = f'torch_gru_{hidden_dim}hid_{batch_size}batch_{epoch}epochs_losses.png'
+    fig.savefig(os.path.join(path, image_name))
+
+
+# def evaluate(model, test_x, test_y, label_scalers):
+#     model.eval()
+#     outputs = []
+#     targets = []
+#     start_time = time.time()
+#     for i in test_x.keys():
+#         inp = torch.from_numpy(np.array(test_x[i]))
+#         labs = torch.from_numpy(np.array(test_y[i]))
+#         h = model.init_hidden(inp.shape[0])
+#         out, h = model(inp.to(device).float(), h)
+#         outputs.append(label_scalers[i].inverse_transform(out.cpu().detach().numpy()).reshape(-1))
+#         targets.append(label_scalers[i].inverse_transform(labs.numpy()).reshape(-1))
+#     print("Evaluation Time: {}".format(str(time.time()-start_time)))
+#     sMAPE = 0
+#     for i in range(len(outputs)):
+#         sMAPE += np.mean(abs(outputs[i]-targets[i])/(targets[i]+outputs[i])/2)/len(outputs)
+#     print("sMAPE: {}%".format(sMAPE*100))
+#     return outputs, targets, sMAPE
