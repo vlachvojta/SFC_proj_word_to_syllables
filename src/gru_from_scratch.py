c8d8489e28a74e3d016de0ec81fbef1748746491
vlachvojta <vojtik.vlach@seznam.cz>
2023-11-27 18:12:26 +0100

    Update with gru_pytorch stuff

--- src/gru_from_scratch.py
+++ src/gru_from_scratch.py
@@ -91,6 +91,7 @@ class GRUNetFromScratch():
         print(f'gru_x_weights ({self.gru_x_weights.shape}):\n{self.gru_x_weights}')
 
         z_t = torch.sigmoid(torch.mm(self.gru_x_weights[0], x) + torch.mm(self.W_hz, h_prev))  # By ChatGPT...
+
         u_z_k_t = torch.matmul(self.gru_x_weights[0], x) + self.gru_h_weights[0] * h_prev
         print(f'shape of torch.matmul(self.gru_x_weights[0], x): {torch.matmul(self.gru_x_weights[0], x).shape}')
         asdf = self.gru_h_weights[0] * h_prev
@@ -110,27 +111,32 @@ class GRUNetFromScratch():
         h = (1 - z_k_t) * h + z_k_t * h_k_t
         return h
 
-    def init_hidden(self): #, batch_size):
-        hidden = torch.zeros(self.hidden_dim) # torch.zeros(batch_size, self.hidden_dim)  # For batch training
-        print('hidden:')
-        print(hidden.shape)
-        print(hidden)
+    def init_hidden(self, batch_size:int=None):
+        if not batch_size:
+            hidden = torch.zeros(self.hidden_dim, dtype=torch.float)
+        hidden = torch.zeros(batch_size, self.hidden_dim) # torch.zeros(batch_size, self.hidden_dim)  # For batch training
+        print(f'init_hidden: {hidden.shape} zeros...')
         return hidden
 
+def train(train_data: Dataset, val_data: Dataset, learn_rate, hidden_dim=8, epochs=5, device='cpu',
+          batch_size=1, save_step=5, view_step=1):
+    model = GRUNetFromScratch(hidden_dim=hidden_dim, device=device, batch_size=batch_size, output_dim=1)
 
-def train(train_data: Dataset, learn_rate, hidden_dim=8, epochs=5, device='cpu', batch_size=32):
-    model = GRUNetFromScratch(hidden_dim=hidden_dim, device=device, batch_size=batch_size)
     print("Starting Training")
+    print('')
     epoch_times = []
+    trn_losses = []
+    trn_losses_lev = []
+    val_losses_lev = []
+    h = model.init_hidden(batch_size)
 
     for epoch in range(1, epochs + 1):
         start_time = time.time()
-        losses = []
-        for i, (x, labels) in enumerate(train_data.batch_iterator(batch_size, tensor_output=True)):
+        for i, (x, labels) in enumerate(train_data.batch_iterator(batch_size, tensor_output=True), start=1):
             print(f'x({x.shape}):')
             print(f'labels({labels.shape})')
-            
-            out, _ = model(x)
+
+            out, _ = model(x, h)
             print(f'out({out.shape}):')
             print(out)
             # Get out loss
@@ -142,8 +148,8 @@ def train(train_data: Dataset, learn_rate, hidden_dim=8, epochs=5, device='cpu',
             print(labels[:5])
 
             # TODO calculate loss using levenstein distance
-            losses.append(helpers.levenstein_loss(outputs, labels))
-            print(f'Loss: {losses[-1]:.3f} %')
+            trn_losses_lev.append(helpers.levenstein_loss(outputs, labels))
+            print(f'Loss: {trn_losses_lev[-1]:.3f} %')
 
             # TODO backpropagate and update weights
             print('Here you should start updating weights and stuff...')
@@ -154,35 +160,45 @@ def train(train_data: Dataset, learn_rate, hidden_dim=8, epochs=5, device='cpu',
             loss.backward()
             optimizer.step()
             losses.append(loss.item())
-            if i%200 == 0:
-                print("Epoch {}......Step: {}/{}....... Average Loss for Epoch: {}".format(epoch, i, len(train_loader), np.mean(losses)))
+            trn_losses_lev.append(helpers.levenstein_loss(outputs, labels))
+        trn_losses.append(loss.item())
         current_time = time.time()
-        print("Epoch {}/{} Done, Total Loss: {}".format(epoch, epochs, avg_loss/len(train_loader)))
-        print("Time Elapsed for Epoch: {} seconds".format(str(current_time-start_time)))
+
+        if epoch % view_step == 0:
+            val_loss_lev, val_out_words, val_labels_words = helpers.test_val(model, val_data, device, batch_size)
+            val_losses_lev.append(val_loss_lev)
+
+            print(f"Epoch {epoch}/{epochs}, trn losses: {trn_losses[-1]:.3f}, {trn_losses_lev[-1]:.2f} %, val losses: {val_losses_lev[-1]:.2f} %")
+            print(f"Time Elapsed for Epoch: {current_time - start_time:.2f} seconds")
+            print('Example:')
+            print(f'\tout: {val_out_words[:100]}')
+            print(f'\tlab: {val_labels_words[:100]}')
+            print('')
         epoch_times.append(current_time-start_time)
-    print("Total Training Time: {} seconds".format(str(sum(epoch_times))))
+
+        if epoch % save_step == 0:
+            helpers.plot_losses(trn_losses, trn_losses_lev, val_losses_lev, hidden_dim, epoch, batch_size, view_step=view_step, path='models')
+            helpers.save_model(model, hidden_dim, epoch, batch_size, path='models')
+            helpers.save_out_and_labels(val_out_words, val_labels_words, hidden_dim, epoch, batch_size, path='models')
+
+    print(f"Total Training Time: {sum(epoch_times):.2f} seconds. ({sum(epoch_times)/epochs:.2f} seconds per epoch)")
+
     return model
-
-
-def evaluate(model):
-    pass
 
 
 def main():
     trn_file = os.path.join("dataset", "ssc_29-06-16", "set_1000_250", "trn.txt")
     val_file = os.path.join("dataset", "ssc_29-06-16", "set_1000_250", "val.txt")
 
-    trn_dataset = Dataset(trn_file)
-    val_dataset = Dataset(val_file)
+    padding = 20
 
+    trn_dataset = Dataset(trn_file, rnn_shift=0, padding=padding)
+    val_dataset = Dataset(val_file, rnn_shift=0, padding=padding)
     print(f'Loaded {len(trn_dataset)} training and {len(val_dataset)} validation samples. Such as:')
-    for i in range(5):
-        print('\t', trn_dataset[i])
-
-    batch_size = 32
+    
     device = torch.device("cuda") if torch.cuda.is_available() else "cpu"
 
-    gru_model = train(trn_dataset, learn_rate=0.001, device=device, batch_size=batch_size)
+    gru_model = train(trn_dataset, val_dataset, learn_rate=0.001, device=device, batch_size=1, epochs=500, save_step=50, view_step=5)
     # gru_outputs, targets, gru_sMAPE = evaluate(gru_model, test_x, test_y, label_scalers)
 
 
