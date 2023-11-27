3777313af09519ba2f9c17efb50914c857730e6a
vlachvojta <vojtik.vlach@seznam.cz>
2023-11-27 01:25:32 +0100

    Add base for a lot of stuff, but nothing is working so far...

--- src/gru_from_scratch.py
+++ src/gru_from_scratch.py
@@ -10,20 +10,35 @@ import matplotlib.pyplot as plt
 
 import torch
 from torch import nn
+import Levenshtein as lev
 
 from dataset import Dataset
+import charset
 
 
-class GRUNetFromScratch(nn.Module):
-    def __init__(self, input_dim=1, hidden_dim=256, output_dim=1, device='cpu', batch_size=32):
+class GRUNetFromScratch():
+    def __init__(self, input_dim=1, hidden_dim:int = 8, output_dim=1, device='cpu', batch_size=32):
+        self.input_dim = input_dim
         self.hidden_dim = hidden_dim
+        self.output_dim = output_dim
         self.device = device
         self.batch_size = batch_size
 
-        self.weights = torch.zeros(4, hidden_dim)
-        torch.nn.init.xavier_uniform_(self.weights)
-
         # TODO create weights for gru layer
+        self.gru_x_weights = torch.zeros(3, hidden_dim)  # weights for w_zk, w_rk, w_hk
+        self.gru_h_weights = torch.zeros(3)  # weights for v_zk, v_rk, v_hk
+        self.fc_weights = torch.zeros(hidden_dim, output_dim)
+        torch.nn.init.xavier_uniform_(self.gru_x_weights)
+        print('gru_x_weights:')
+        print(self.gru_x_weights.shape)
+        print(self.gru_x_weights)
+        print('gru_h_weights:')
+        print(self.gru_h_weights.shape)
+        print(self.gru_h_weights)
+        print('fc_weights:')
+        print(self.fc_weights.shape)
+        print(self.fc_weights)
+
         # self.gru = nn.GRU(input_dim, hidden_dim, batch_first=True, dropout=drop_prob)
 
         # TODO create weights for fc layer
@@ -34,53 +49,123 @@ class GRUNetFromScratch(nn.Module):
 
     def __str__(self):
         return (f'GRUNetFromScratch(input_dim={self.input_dim}, hidden_dim={self.hidden_dim}, '
-                f'output_dim={self.output_dim}, drop_prob={self.drop_prob}, device={self.device}, batch_size={self.batch_size})')
+                f'output_dim={self.output_dim}, device={self.device}, batch_size={self.batch_size})')
 
-    def forward(self, x, h):
+    def __call__(self, x, h=None):
+        if h is None:
+            h = self.init_hidden()
+        return self.forward(x, h)
+
+    def forward(self, x_batch, h):
+        """Get output and hidden state from GRU network."""
+
+        # TODO go though the network and cycle through len of input = count of grus (forward passes)
+        out = []
+        for word in x_batch:
+            out.append([])
+            for letter in word:
+                # print(f'type of letter: {type(letter.item())}')
+                h = self.gru_cell(letter.item(), h)
+                # out.append(self.fc(self.relu(h)))
+                # TODO out[-1] append linear(activate(current_h))
+
         # TODO rewrite this for from scratch implementation
-        print(f'FORWARD! x({x.shape}): {x}')
-        out, h = self.gru(x, h)
-        print(f'out({out.shape}): {out}')
-        print(f'h({h.shape}): {h}')
-        out = self.fc(self.relu(out[:,-1]))
-        print(f'fc(relu(out)) ({out.shape}): {out}')
+        # print(f'FORWARD! x({x.shape}): {x}')
+        # out, h = self.gru(x, h)
+        # print(f'out({out.shape}): {out}')
+        # print(f'h({h.shape}): {h}')
+        # out = self.fc(self.relu(out[:,-1]))
+        # print(f'fc(relu(out)) ({out.shape}): {out}')
         return out, h
 
+    def gru_cell(self, x, h_prev):
+        """GRU cell implementation."""
+        x = torch.full((self.input_dim, 1), x, dtype=torch.float)
+        # x = torch.full((self.hidden_dim, 1), x, dtype=torch.float)  # TODO This may be wrong, check later
+        # self.gru_h_weights for v_zk, v_rk, v_hk
+        # self.gru_x_weights for w_zk, w_rk, w_hk
 
-def train(train_data: Dataset, learn_rate, hidden_dim=256, EPOCHS=5, device='cpu', batch_size=32):
-    # Instantiating the models
-    model = GRUNetFromScratch()
-    model.to(device)
+        print('')
+        print(f'x ({x.shape}):\n{x}')
+        print(f'h_prev ({h_prev.shape}):\n{h_prev}')
+        print(f'gru_x_weights ({self.gru_x_weights.shape}):\n{self.gru_x_weights}')
 
-    # Defining loss function and optimizer
-    criterion = nn.MSELoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=learn_rate)
+        z_t = torch.sigmoid(torch.mm(self.gru_x_weights[0], x) + torch.mm(self.W_hz, h_prev))  # By ChatGPT...
+        u_z_k_t = torch.matmul(self.gru_x_weights[0], x) + self.gru_h_weights[0] * h_prev
+        print(f'shape of torch.matmul(self.gru_x_weights[0], x): {torch.matmul(self.gru_x_weights[0], x).shape}')
+        asdf = self.gru_h_weights[0] * h_prev
+        print(f'shape of self.gru_h_weights[0] * h_prev: {asdf.shape}')
+        print(f'u_z_k_t ({u_z_k_t.shape}):\n{u_z_k_t}')
+        z_k_t = torch.sigmoid(u_z_k_t)
+        print(f'z_k_t ({z_k_t.shape}):\n{z_k_t}')
 
-    model.train()
+        u_r_k_t = torch.matmul(self.gru_x_weights[1], x) + self.gru_h_weights[1] * h_prev
+
+
+        exit(0)
+        
+        # z_k_t = torch.sigmoid(torch.matmul(self.weights[0], torch.cat((x, h), dim=0)))
+        # r_k_t = torch.sigmoid(torch.matmul(self.weights[1], torch.cat((x, h), dim=0)))
+        # h_k_t = torch.tanh(torch.matmul(self.weights[2], torch.cat((x, r_k_t * h), dim=0)))
+        h = (1 - z_k_t) * h + z_k_t * h_k_t
+        return h
+
+    def init_hidden(self): #, batch_size):
+        hidden = torch.zeros(self.hidden_dim) # torch.zeros(batch_size, self.hidden_dim)  # For batch training
+        print('hidden:')
+        print(hidden.shape)
+        print(hidden)
+        return hidden
+
+
+def levenstein_loss(a, b):
+    if isinstance(a, list):
+        a = ''.join(a)
+    if isinstance(b, list):
+        b = ''.join(b)
+    return 100.0 * lev.distance(a, b) / len(b)
+
+
+def train(train_data: Dataset, learn_rate, hidden_dim=8, epochs=5, device='cpu', batch_size=32):
+    model = GRUNetFromScratch(hidden_dim=hidden_dim, device=device, batch_size=batch_size)
     print("Starting Training")
     epoch_times = []
-    # Start training loop
-    for epoch in range(1,EPOCHS+1):
+
+    for epoch in range(1, epochs + 1):
         start_time = time.time()
-        h = model.init_hidden(batch_size)
-        avg_loss = 0.
-        counter = 0
-        for x, label in train_data.batch_iterator(batch_size, tensor_output=True):
+        losses = []
+        for i, (x, labels) in enumerate(train_data.batch_iterator(batch_size, tensor_output=True)):
             print(f'x({x.shape}):')
-            print(f'label({label.shape})')
-            counter += 1
-            h = h.data
-            model.zero_grad()
+            print(f'labels({labels.shape})')
             
-            out, h = model(x.to(device).float(), h)
-            loss = criterion(out, label.to(device).float())
+            out, _ = model(x)
+            print(f'out({out.shape}):')
+            print(out)
+            # Get out loss
+            outputs = [charset.tensor_to_word(o) for o in out]
+            print(f'outputs({len(outputs)}):')
+            print(outputs[:5])
+            labels = [charset.tensor_to_word(l) for l in labels]
+            print(f'labels({len(labels)}):')
+            print(labels[:5])
+
+            # TODO calculate loss using levenstein distance
+            losses.append(levenstein_loss(outputs, labels))
+            print(f'Loss: {losses[-1]:.3f} %')
+
+            # TODO backpropagate and update weights
+            print('Here you should start updating weights and stuff...')
+            exit(0)
+
+
+            loss = criterion(out, labels.to(device).float())
             loss.backward()
             optimizer.step()
-            avg_loss += loss.item()
-            if counter%200 == 0:
-                print("Epoch {}......Step: {}/{}....... Average Loss for Epoch: {}".format(epoch, counter, len(train_loader), avg_loss/counter))
+            losses.append(loss.item())
+            if i%200 == 0:
+                print("Epoch {}......Step: {}/{}....... Average Loss for Epoch: {}".format(epoch, i, len(train_loader), np.mean(losses)))
         current_time = time.time()
-        print("Epoch {}/{} Done, Total Loss: {}".format(epoch, EPOCHS, avg_loss/len(train_loader)))
+        print("Epoch {}/{} Done, Total Loss: {}".format(epoch, epochs, avg_loss/len(train_loader)))
         print("Time Elapsed for Epoch: {} seconds".format(str(current_time-start_time)))
         epoch_times.append(current_time-start_time)
     print("Total Training Time: {} seconds".format(str(sum(epoch_times))))
@@ -98,19 +183,12 @@ def main():
     trn_dataset = Dataset(trn_file)
     val_dataset = Dataset(val_file)
 
-    print(f'Loaded {len(trn_dataset)} training and {len(val_dataset)} validation samples.')
-    for i in range(10):
-        print(trn_dataset[i])
+    print(f'Loaded {len(trn_dataset)} training and {len(val_dataset)} validation samples. Such as:')
+    for i in range(5):
+        print('\t', trn_dataset[i])
 
     batch_size = 32
     device = torch.device("cuda") if torch.cuda.is_available() else "cpu"
-
-    for batch in trn_dataset.batch_iterator(batch_size, tensor_output=True):
-        print(f'batch inputs: ({batch[0].shape})')
-        print(f'batch targets: ({batch[1].shape})')
-        print(batch[0])
-        print(batch[1])
-        break
 
     gru_model = train(trn_dataset, learn_rate=0.001, device=device, batch_size=batch_size)
     # gru_outputs, targets, gru_sMAPE = evaluate(gru_model, test_x, test_y, label_scalers)
