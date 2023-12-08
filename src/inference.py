7b5835e0370b8771d6f5d5b5506e357ec07f0dd7
vlachvojta <vojtik.vlach@seznam.cz>
2023-12-08 01:59:40 +0100

    Sort model dirs

--- src/inference.py
+++ src/inference.py
@@ -5,11 +5,15 @@ from net_definitions import *
 
 def main():
     model, _ = helpers.load_model(GRUNetBinaryEmbeding, 
-        'models/020_gru_binary_emb_256h_29000data_dropout_slower_testing')
+        'models/025_gru_binary_emb_256h_29000data_dropout_slower_both')
         # 'models/022_gru_binary_emb_256h_29000data_dropout_slower_bigger_test')
-        # 'models/019_gru_binary_emb_256h_29000data_dropout/torch_gru_256hid_250batch_500epochs.pt')
-        # 'models/020_gru_binary_emb_256h_29000data_dropout_slower')
         # 'models/021_gru_binary_emb_256h_29000data_dropout_slower_bigger')
+        # 'models/020_gru_binary_emb_256h_29000data_dropout_slower_testing')
+        # 'models/020_gru_binary_emb_256h_29000data_dropout_slower')
+        # 'models/019_gru_binary_emb_256h_29000data_dropout/torch_gru_256hid_250batch_500epochs.pt')
+
+    if not model:
+        raise FileNotFoundError('No model found.')
 
     model.eval()
     print('Model:')
